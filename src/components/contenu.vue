17c529bb62d7521eb5708c7f8efad33ec5bf83d3
1464572 <hovington234@gmail.com>
2021-03-18 23:15:27 -0400

    Formulaire terminer débutant

--- src/components/contenu.vue
+++ src/components/contenu.vue
@@ -5,7 +5,7 @@
     <form class=" flex rounded-md grid grid-cols-6 gap-6 bg-red-300 h-full" action="">
         <div class="m-2 col-span-6 sm:col-span-3">
             <label for="prenom" class="block text-sm font-medium text-gray-700">Ton prénom</label>
-            <input v-model.lazy='formData.prenom' type="text" id='prenom' class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+            <input v-on:input='toggleResult' v-model.lazy='formData.prenom' type="text" id='prenom' class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
         </div>
         <div class="m-2 col-span-6 sm:col-span-3">
             <label for="nom" class="block text-sm font-medium text-gray-700">Ton nom</label>
@@ -24,6 +24,7 @@
         </select>
 
         <div class="flex m-2 col-span-6 sm:col-span-4">
+            <h2 class="text-2xl col-span-6 sm:col-span-6">checkbox</h2>
             <div class=" m-2 col-span-6 sm:col-span-1">
                 <input v-model="formData.checkFruits" value='fraise' id="fraise" type="checkbox" class="block text-sm font-medium text-gray-700">
                 <label class="block text-sm font-medium text-gray-700" for="fraise"> Coche la fraise</label>
@@ -38,6 +39,10 @@
             </div>
         </div>
 
+        <button v-on:click.prevent='envoiForm' class="bg-blue-500 sm:col-span-6 col-span-6 rounded-lg px-6 py-2 mb-2  m-auto ">Envoyer le formulaire</button>
+    </form>
+    <br>
+    <div class="border-2 border-gray-700 rounded-lg " v-if='infoSubmit'>
         <h2 class="text-3xl col-span-6 sm:col-span-6">Résultats</h2>
         <div class="m-2 col-span-6 sm:col-span-6">
             <p>Prénom :{{formData.prenom}} </p>
@@ -48,10 +53,10 @@
                 <li class="text-purple-700" v-bind:key="index" v-for="(fruits,index) in formData.checkFruits"> {{fruits}}</li>
             </ul>
 
-        <p>Choix du select : {{formData.select}}</p>
+            <p>Choix du select : {{formData.select}}</p>
 
         </div>
-    </form>
+    </div>
 
 </div>
 </template>
@@ -67,10 +72,19 @@ export default ({
                 checkFruits: [
 
                 ],
-                select:'',
-                listePays:['Russie','Canada','USA', 'France','Japon','Mexique'],
+                select: '',
+                listePays: ['Russie', 'Canada', 'USA', 'France', 'Japon', 'Mexique'],
 
-            }
+            },
+            infoSubmit: false,
+        }
+    },
+    methods:{
+        envoiForm:function(){
+            this.infoSubmit=true;
+        },
+        toggleResult:function(){
+            this.infoSubmit=false;
         }
     }
 })
